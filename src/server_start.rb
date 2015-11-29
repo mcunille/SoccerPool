2f1569a8889522125b74485e78c4420fb1a8c4e6
daniortiz-bj <danielaortiz_bjagd@hotmail.com>
2015-11-28 22:11:26 -0600

    server_start

---
+++ src/server_start.rb
@@ -0,0 +1,78 @@
+# Final Project: Soccer Pool
+# Date: 30-Nov-2015
+# Authors: A01164759 Mauricio Cunillé Blando
+#          A01169513 Daniela Ortiz Rodríguez
+
+require 'sinatra'
+require 'controllers/account'
+require 'controllers/pool'
+require 'controllers/scores'
+
+
+
+configure do
+  enable :sessions
+end
+
+get '/account/login' do
+  
+end
+
+post '/account/login' do
+  
+  
+end
+
+get '/account/register' do
+  
+end
+
+post '/account/register' do
+  
+  
+end
+
+get '/pool/new' do
+  
+end
+
+post '/pool/new' do
+  
+  
+end
+
+get '/pool/close' do
+  
+end
+
+post '/pool/close' do
+  
+  
+end
+
+get '/pool/pick' do
+  
+end
+
+post '/pool/pick' do
+  
+  
+end
+
+get '/pool/results' do
+  
+end
+
+post '/pool/results' do
+  
+  
+end
+
+get '/scores' do
+  
+end
+
+post '/scores' do
+  
+  
+end
