692b995a830da877a648c1af8ca07f407017db70
Mauricio Cunille <m.cunille@outlook.com>
2015-11-29 02:43:06 -0600

    Working on controllers.

--- src/server_start.rb
+++ src/server_start.rb
@@ -4,75 +4,142 @@
 #          A01169513 Daniela Ortiz Rodríguez
 
 require 'sinatra'
-require 'controllers/account'
-require 'controllers/pool'
-require 'controllers/scores'
-
-
+require 'sinatra/content_for'
+require_relative 'models/Account'
+require_relative 'models/Match'
+require_relative 'models/Pick'
+require_relative 'models/Pool'
 
 configure do
   enable :sessions
+  Account.create_table
+  Pool.create_table
+  Match.create_table
+  Pick.create_table
+  
+  begin
+      admin = Account.new("admin", "Admin@123456", "admin")
+      admin.save
+  rescue
+      #ignore
+  end
 end
 
-get '/account/login' do
+get '/' do
+    if session == nil || session[:login] == nil
+            redirect '/login'
+    end
+    
+    redirect '/scores'
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  login = params[:login]
+  password = params[:password]
+  user = Account.find_by_login(login)
+  
+  if (user != nil && user.password == password)
+    session[:login] = user
+    user.role == 'admin' ? redirect('/open') : redirect('/scores')
+  end
+  
+  @error = "Wrong username and/or password"
+  erb :login
+end
+
+get '/register' do
+  erb :register
+end
+
+post '/register' do
+  login = params[:login]
+  password = params[:password]
+  password_c = params[:password_confirm]
+  
+  if password != password_c
+    @error = 'The passwords don\'t match'
+    puts @error
+    erb :register
+  elsif Account.find_by_login(login)
+    @error = 'That login already exists'
+    puts @error
+    erb :register
+  else  
+    user = Account.new(login, password, 'user')
+    user.save
+    session[:login] = user
+    user.role == 'admin' ? redirect('/open') : redirect('/scores')
+  end 
+end
+
+get '/open' do
+    login = session[:login]
+    puts login
+    if login == nil || login.role != 'admin'
+      redirect '/'
+    end
+  
+    erb :openPool
+end
+
+post '/open' do
+    pool = Pool.new(Pool::OPEN)
+    pool.save
+    
+    teams = {}
+    
+    params.each {|e|
+        data = e[0].split('_')
+        if teams[data[1]] == nil
+            teams[data[1]] = Match.new(nil, nil, Match::NO_RESULTS, pool)
+        end
+        
+        if data[0].end_with?('A')
+            teams[data[1]].firstTeam = e[1]
+        else
+            teams[data[1]].secondTeam = e[1]
+        end
+    }
+    
+    teams.each {|e|
+        e[1].save
+    }
+    
+    redirect '/close'
+end
+
+get '/close' do
+  erb :closePool
+end
+
+post '/close' do
   
 end
 
-post '/account/login' do
-  
+get '/pick' do
+  erb :pick
+end
+
+post '/pick' do
   
 end
 
-get '/account/register' do
+get '/results' do
   
 end
 
-post '/account/register' do
-  
-  
-end
-
-get '/pool/new' do
-  
-end
-
-post '/pool/new' do
-  
-  
-end
-
-get '/pool/close' do
-  
-end
-
-post '/pool/close' do
-  
-  
-end
-
-get '/pool/pick' do
-  
-end
-
-post '/pool/pick' do
-  
-  
-end
-
-get '/pool/results' do
-  
-end
-
-post '/pool/results' do
-  
+post '/results' do
   
 end
 
 get '/scores' do
-  
+  erb :scores
 end
 
 post '/scores' do
-  
   
 end
